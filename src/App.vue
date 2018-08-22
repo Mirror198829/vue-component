78e333b011054aa48d62a982d6bcfd4d7018924c
Mirror198829 <412366756@qq.com>
2018-08-22 10:43:07 +0800

    修改代码

--- src/App.vue
+++ src/App.vue
@@ -1,118 +1,57 @@
 <template>
   <div id="app">
-    <div class="parentCpt">
-        <h1>父组件</h1>
-        <el-input style="margin-top:10px" v-model="name.data" placeholder="地址传递数据给子组件"></el-input>
-        <el-input style="margin-top:10px" v-model="data1" placeholder="值传递数据给子组件"></el-input>
-        <el-input style="margin-top:10px" v-model="grandson.data" placeholder="地址传递给孙子组件"></el-input>
-        <button >我是showNum按钮</button>
-        <div class="childCptWrap">         
-          <v-header  :my-message="name" @getnum="showNum" :my-data="data1" :my-grandson="grandson"></v-header>
-          <div class="clear" style="width:100%">
-            <div style="float:left;" class="cptWrap">
-              <v-component-a></v-component-a>
-            </div>
-            <div style="float:right" class="cptWrap">
-              <v-component-b></v-component-b>           
-            </div>
-          </div>         
-          <my-component></my-component>
-          <slot-a v-show="true">
-            <p slot="CPU">CPU</p>
-            <p slot="membory">内存</p>
-            <p>我是另外的slot</p>
-          </slot-a>
-        </div>
-        <div>
-          <h4>我是App.vue中vuex的值</h4>
-          <em>{{count}}</em>
-          <button @click="changeCount">改变state</button>
-          <button @click="transCount('vuex')">传入字符串参数</button>
-          <input type="text" @input="changeTxt"/>
-        </div>
-        <vuex-child-cpt></vuex-child-cpt>
-        <v-draggable></v-draggable>
-        
+    <el-menu
+      :router="true"
+      :default-active="$route.path"
+      mode="horizontal"
+      backgroundColor="#545c64"
+      text-color="#fff"
+      active-text-color="#ffd04b"
+      >
+      <el-menu-item index="/">组件</el-menu-item>
+      <el-menu-item index="/drag">拖拽插件</el-menu-item>
+    </el-menu>
+    <div  class="main">
+      <router-view></router-view>
     </div>
+    <footer id="foot">
+       <p>江苏南京 前端开发工程师 曹静</p>
+       <p><a href="https://github.com/Mirror198829" target="_blank">https://github.com/Mirror198829</a></p>
+    </footer>
   </div>
 </template>
-
 <script>
-import vheader from './components/v-header.vue'
-import box from './components/box.vue'
-import vComponentA from './components/v-component-a.vue'
-import vComponentB from './components/v-component-b.vue' 
-import slotA from './components/slot-a.vue'
-import vuexChildCpt from './components/vuex-child-component.vue'
-import vDraggable from './components/v-draggable.vue'
 export default {
-  name: 'app',
-  data(){
-    return{
-      name:{data:''},
-      data1:'',
-      grandson:{data:''},
-      info:'看看我出来了没有',
+  name: '',
+  data () {
+    return {
       
     }
-  }, 
-  computed: {
-      count () {
-          return this.$store.state.count
-      },
-      dragOptions () {
-        return  {
-          animation:2,
-          group: 'people',
-          ghostClass: 'ghost'
-        };
-      },
-  }, 
-  components:{
-    vHeader:vheader,
-    box:box,
-    vComponentA:vComponentA,
-    vComponentB:vComponentB,
-    slotA:slotA,
-    vuexChildCpt:vuexChildCpt,
-    vDraggable
   },
   methods:{
-    showNum(abc){
-      alert(abc)
-    },
-    changeCount(){
-      this.$store.commit('increment')
-    },
-    transCount(arg){
-      this.$store.commit('changeContent',arg)
-    },
-    changeTxt(){
-      console.log(123130)
-    }
+
   },
-  watch: {
-    isDragging (newValue) {
-      console.log(123)
-      if (newValue){
-        this.delayedDragging= true
-        return
-      }
-      this.$nextTick( () =>{
-           this.delayedDragging =false
-      })
-    }
-  }
+  mounted(){},
+  created(){}
 }
 </script>
-
-<style>
+<style  lang="less">
+@import '../less/index.less';
 *{margin:0;padding:0;}
-body{background-color: #1abc9c}
-.parentCpt{padding:30px;width: 1000px;margin:60px auto;background-color: #eceff1;border-radius: 10px;}
-.childCptWrap{}
-#app {font-family: 'Avenir', Helvetica, Arial, sans-serif;-webkit-font-smoothing: antialiased;-moz-osx-font-smoothing: grayscale;text-align: center;color: #2c3e50;}
+html,body{width:100%;height:100%;}
+body{background-color:#fff}
+a{text-decoration: none;color:@font-color;}
+#app {display: flex;flex-direction: column;height:100%;}
+.main{flex:1;overflow:auto;background-color:#c9d6df}
 .clear{ zoom:1;}
 .clear:after{ content:''; display:block; clear:both;}
-.cptWrap{border-radius:10px;overflow: hidden;width:49%;margin-top:20px}
+</style>
+<style scoped  lang="less">
+@import '../less/index.less';
+#foot{padding:15px;text-align: center;font-size:14px;background-color:@base-color;color:@font-color;
+  p:first-child{padding-bottom:2px;}
+  a{
+    &:hover{text-decoration:underline;}
+  }
+}
 </style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,101 @@
+<template>
+  <div>
+     <div class="parentCpt">
+        <h1>父组件</h1>
+        <el-input style="margin-top:10px" v-model="name.data" placeholder="地址传递数据给子组件"></el-input>
+        <el-input style="margin-top:10px" v-model="data1" placeholder="值传递数据给子组件"></el-input>
+        <el-input style="margin-top:10px" v-model="grandson.data" placeholder="地址传递给孙子组件"></el-input>
+        <button >我是showNum按钮</button>
+        <div class="childCptWrap">         
+          <v-header  :my-message="name" @getnum="showNum" :my-data="data1" :my-grandson="grandson"></v-header>
+          <div class="clear" style="width:100%">
+            <div style="float:left;" class="cptWrap">
+              <v-component-a></v-component-a>
+            </div>
+            <div style="float:right" class="cptWrap">
+              <v-component-b></v-component-b>           
+            </div>
+          </div>         
+          <my-component></my-component>
+          <slot-a v-show="true">
+            <p slot="CPU">CPU</p>
+            <p slot="membory">内存</p>
+            <p>我是另外的slot</p>
+          </slot-a>
+        </div>
+        <div>
+          <h4>我是App.vue中vuex的值</h4>
+          <em>{{count}}</em>
+          <button @click="changeCount">改变state</button>
+          <button @click="transCount('vuex')">传入字符串参数</button>
+          <input type="text" @input="changeTxt"/>
+        </div>
+        <vuex-child-cpt></vuex-child-cpt>       
+    </div>   
+  </div>
+</template>
+
+<script>
+import vheader from '@/components/v-header.vue'
+import box from '@/components/box.vue'
+import vComponentA from '@/components/v-component-a.vue'
+import vComponentB from '@/components/v-component-b.vue' 
+import slotA from '@/components/slot-a.vue'
+import vuexChildCpt from '@/components/vuex-child-component.vue'
+export default {
+  name: 'Home',
+  data(){
+    return{
+      name:{data:''},
+      data1:'',
+      grandson:{data:''},
+      info:'看看我出来了没有',
+      
+    }
+  }, 
+  computed: {
+      count () {
+          return this.$store.state.count
+      },
+      dragOptions () {
+        return  {
+          animation:2,
+          group: 'people',
+          ghostClass: 'ghost'
+        };
+      },
+  }, 
+  components:{
+    vHeader:vheader,
+    box:box,
+    vComponentA:vComponentA,
+    vComponentB:vComponentB,
+    slotA:slotA,
+    vuexChildCpt:vuexChildCpt
+  },
+  methods:{
+    showNum(abc){
+      alert(abc)
+    },
+    changeCount(){
+      this.$store.commit('increment')
+    },
+    transCount(arg){
+      this.$store.commit('changeContent',arg)
+    },
+    changeTxt(){
+      console.log(123130)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+@import '../../less/index.less';
+.parentCpt{padding:30px;width: 1000px;margin:60px auto;border-radius: 10px;background-color:@base-color;}
+.childCptWrap{margin-bottom:10px;}
+</style>
+<style  lang="less">
+
+</style>
